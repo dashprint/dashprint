c893a563dde0afb068cd55764ce495365b3d845a
Lubos Dolezel <lubos@dolezel.info>
2019-09-09 09:38:07 +0200

    Bug fixes

--- src/Printer.cpp
+++ src/Printer.cpp
@@ -436,6 +436,39 @@ void Printer::doRead()
 		std::bind(&Printer::readDone, this, std::placeholders::_1));
 }
 
+static std::istream& safeGetline(std::istream& is, std::string& t)
+{
+    t.clear();
+
+    // The characters in the stream are read one-by-one using a std::streambuf.
+    // That is faster than reading them one-by-one using the std::istream.
+    // Code that uses streambuf this way must be guarded by a sentry object.
+    // The sentry object performs various tasks,
+    // such as thread synchronization and updating the stream state.
+
+    std::istream::sentry se(is, true);
+    std::streambuf* sb = is.rdbuf();
+
+    for(;;) {
+        int c = sb->sbumpc();
+        switch (c) {
+        case '\n':
+            return is;
+        case '\r':
+            if(sb->sgetc() == '\n')
+                sb->sbumpc();
+            return is;
+        case std::streambuf::traits_type::eof():
+            // Also handle the case when the last line has no line ending
+            if(t.empty())
+                is.setstate(std::ios::eofbit);
+            return is;
+        default:
+            t += (char)c;
+        }
+    }
+}
+
 void Printer::readDone(const boost::system::error_code& ec)
 {
 	if (ec)
@@ -447,7 +480,7 @@ void Printer::readDone(const boost::system::error_code& ec)
 	std::istream is(&m_streamBuf);
 	std::string line;
 
-	std::getline(is, line);
+	safeGetline(is, line);
 
 	m_lastIncomingData = std::chrono::steady_clock::now();
 
@@ -495,17 +528,19 @@ void Printer::readDone(const boost::system::error_code& ec)
 		{
 			m_nextLineNo = 1;
 		}
-
-		if (resendLine != -1)
-			handleResend(resendLine);
 		else
 		{
-			if (!m_commandQueue.empty())
+			if (resendLine != -1)
+				handleResend(resendLine);
+			else
 			{
-				auto cb = m_commandQueue.front().callback;
-				if (cb)
-					cb(m_replyLines);
-				m_commandQueue.pop();
+				if (!m_commandQueue.empty())
+				{
+					auto cb = m_commandQueue.front().callback;
+					if (cb)
+						cb(m_replyLines);
+					m_commandQueue.pop();
+				}
 			}
 		}
 
